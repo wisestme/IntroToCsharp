04cd118d4636ba96a1390558ccb5f27636e83cf7
wisestMe <wyhzest@gmail.com>
2020-12-30 10:18:24 +0100

    Add project files.

---
+++ IntroToCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace IntroToCsharp
+{
+    class Program
+    {
+        static void AnotherMethod()
+        {
+            Console.WriteLine("Learning C# in another method.");
+        }
+        static void Main()
+        {
+            Console.WriteLine("Learning C# again.");
+            AnotherMethod();
+        }
+    }
+}
