7063e981cdda8743fdfe14209604a70cee568601
wisestMe <wyhzest@gmail.com>
2020-12-31 09:10:46 +0100

    print an element from array

--- IntroToCsharp/Program.cs
+++ IntroToCsharp/Program.cs
@@ -14,49 +14,13 @@ namespace IntroToCsharp
             Console.WriteLine("Learning C# again.");
             AnotherMethod();
 
-            Console.WriteLine("Please what is your name?");
-            string firstName = Console.ReadLine();
-            string lastName = "Nwagwu";
+            int[] EvenNumbers = new int[3];
 
-            Console.WriteLine("Hello {0}, {1}", firstName, lastName);
+            EvenNumbers[0] = 0;
+            EvenNumbers[1] = 2;
+            EvenNumbers[2] = 4;
 
-            int Number = 15;
-
-            bool IsNumber10 = Number == 10 ? true : false;
-
-            Console.WriteLine("Number == 10 is {0}", IsNumber10);
-
-            bool? AreYouMajor = null;
-            if(AreYouMajor == true)
-            {
-                Console.WriteLine("User is a major");
-            }
-            else if(AreYouMajor == false)
-            {
-                Console.WriteLine("User is not a major");
-            }
-            else
-            {
-                Console.WriteLine("User made no selection");
-            }
-
-            //float f = 100.98f;
-            //int i = Convert.ToInt32(f);
-
-            //Console.WriteLine(i);
-
-            string numberString = "100";
-
-            bool ConversionSuccess = int.TryParse(numberString, out int Result);
-            
-            if(ConversionSuccess)
-            {
-                Console.WriteLine(Result);
-            }
-            else
-            {
-                Console.WriteLine("type conversion not posible. please enter a vali number");
-            }
+            Console.WriteLine(EvenNumbers[1]);
         }
     }
 }
