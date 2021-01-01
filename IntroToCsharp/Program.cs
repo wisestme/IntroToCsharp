0b65e2a6823921e0b03e33c397469c7b02298935
wisestMe <wyhzest@gmail.com>
2021-01-01 15:06:26 +0100

    control flow using do while

--- IntroToCsharp/Program.cs
+++ IntroToCsharp/Program.cs
@@ -14,26 +14,43 @@ namespace IntroToCsharp
             Console.WriteLine("Learning C# again.");
             AnotherMethod();
 
-            Console.WriteLine("Please enter your target number");
-
-            string UserTargetString = Console.ReadLine();
-
-            bool ConvertTarget = int.TryParse(UserTargetString, out int UserTarget);
             
-            if(ConvertTarget)
+            string UserAnswer;
+            do
             {
-                int count = 0;
-                while (count < UserTarget)
+                Console.WriteLine("Please enter your target number");
+                string UserTargetString = Console.ReadLine();
+
+                bool ConvertTarget = int.TryParse(UserTargetString, out int UserTarget);
+
+                if (ConvertTarget)
                 {
-                    Console.WriteLine(count);
-                    count++;
+                    int count = 0;
+                    while (count < UserTarget)
+                    {
+                        Console.WriteLine(count);
+                        count++;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number");
                 }
                 
-            }
-            else
-            {
-                Console.WriteLine("Please enter a valid number");
-            }
+                do
+                {
+                    Console.WriteLine("Do you want to continue? -Yes or No ?");
+                    UserAnswer = Console.ReadLine();
+
+                    if (UserAnswer.ToLower() != "yes" && UserAnswer.ToLower() != "no")
+                    {
+                        Console.WriteLine("Invalid choice, please say Yes or No");
+                    }
+                } while (UserAnswer.ToLower() != "yes" && UserAnswer.ToLower() != "no");
+            } while (UserAnswer.ToLower() == "yes");
+            
+            
         }
     }
 }
