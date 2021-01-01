047ae39d4b30e8a0d0fbca05386170d0b7704600
wisestMe <wyhzest@gmail.com>
2021-01-01 09:53:32 +0100

    count up to user target with while loop

--- IntroToCsharp/Program.cs
+++ IntroToCsharp/Program.cs
@@ -14,95 +14,26 @@ namespace IntroToCsharp
             Console.WriteLine("Learning C# again.");
             AnotherMethod();
 
-            Console.WriteLine("Please enter a number");
-            bool ConversionSuccess = int.TryParse(Console.ReadLine(), out int userNumber);
-            if(ConversionSuccess)
-            {
-                if (userNumber % 2 == 0)
-                {
-                    Console.WriteLine("You entered an even number");
-                }
-                else
-                {
-                    Console.WriteLine("You entered an odd number");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please enter a valid number");
-            }
+            Console.WriteLine("Please enter your target number");
 
-            Console.WriteLine("Please enter another number");
+            string UserTargetString = Console.ReadLine();
 
-            bool ConvertSecondNumber = int.TryParse(Console.ReadLine(), out int SecondNumber);
-
-            if (ConvertSecondNumber)
-            {
-                switch (SecondNumber)
-                {
-                    case 10:
-                        
-                    case 20:
-                        
-                    case 30:
-                        Console.WriteLine($"Your second number is {SecondNumber}");
-                        break;
-                    default:
-                        Console.WriteLine("Your number is neither 10, 20 nor 30");
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please enter a valid number");
-            }
-
-            int TotalCoffeeCost = 0;
-            BuyCoffee:
-            Console.WriteLine("Welcome to my coffee shop. please select your choice");
-            Console.WriteLine("1 - Small, 2 - Medium, 3 - Large");
+            bool ConvertTarget = int.TryParse(UserTargetString, out int UserTarget);
             
-            string UserChoiceString = Console.ReadLine();
-
-            int.TryParse(UserChoiceString, out int UserChoice);
-        
-            switch (UserChoice)
+            if(ConvertTarget)
             {
-                case 1:
-                    TotalCoffeeCost += 1;
-                    break;
-                case 2:
-                    TotalCoffeeCost += 2;
-                    break;
-                case 3:
-                    TotalCoffeeCost += 3;
-                    break;
-                default:
-                    Console.WriteLine("invalid selection");
-                    break;
-            }
-
-            Decision:
-            Console.WriteLine("Would you want to buy another coffee? - Yes or No");
-            string BuyAgain = Console.ReadLine();
-
-            if(BuyAgain.ToLower() == "yes")
-            {
-                goto BuyCoffee;
-            }
-            else if(BuyAgain.ToLower() == "no")
-            {
-                goto end;
+                int count = 0;
+                while (count < UserTarget)
+                {
+                    Console.WriteLine(count);
+                    count++;
+                }
+                
             }
             else
             {
-                Console.WriteLine("Invalid selection. Please try again");
-                goto Decision;
+                Console.WriteLine("Please enter a valid number");
             }
-
-            end:
-            Console.WriteLine("Thank you for shopping with us");
-            Console.WriteLine($"Bill Amount = {TotalCoffeeCost}");
         }
     }
 }
