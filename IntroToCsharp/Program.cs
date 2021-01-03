06a5ae00e8d17590256d87c513cabe84837f65fc
wisestMe <wyhzest@gmail.com>
2021-01-03 09:22:26 +0100

    pass parameter by value

--- IntroToCsharp/Program.cs
+++ IntroToCsharp/Program.cs
@@ -14,7 +14,11 @@ namespace IntroToCsharp
             Console.WriteLine("Learning C# again.");
             AnotherMethod();
             Program program = new Program();
-            
+
+            int i = 0;
+            SimpleMethod(i);
+
+            Console.WriteLine(i);
         }
 
         public static void SimpleMethod(int j)
