a2789f699a8405859dcff7f7e56cf47f81c36aee
wisestMe <wyhzest@gmail.com>
2021-01-07 08:20:05 +0100

    override parent class method

--- IntroToCsharp/Program.cs
+++ IntroToCsharp/Program.cs
@@ -8,7 +8,7 @@ namespace IntroToCsharp
         public string FirstName = "FN";
         public string LastName = "LN";
         
-        public void PrintFullName()
+        public virtual void PrintFullName()
         {
             Console.WriteLine($"{FirstName} {LastName}");
         }
@@ -18,17 +18,26 @@ namespace IntroToCsharp
 
     public class PartTimeEmployee : Employee
     {
-
+        public override void PrintFullName()
+        {
+            Console.WriteLine($"{FirstName} {LastName} - Part Time");
+        }
     }
 
     public class FullTimeEmployee : Employee
     {
-
+        public override void PrintFullName()
+        {
+            Console.WriteLine($"{FirstName} {LastName} - Full Time");
+        }
     }
 
     public class TemporaryEmployee : Employee
     {
-
+        public override void PrintFullName()
+        {
+            Console.WriteLine($"{FirstName} {LastName} - Temporal");
+        }
     }
     
     class Program
