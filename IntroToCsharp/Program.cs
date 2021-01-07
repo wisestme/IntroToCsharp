d1485ca370fb525f13247b7c19ce379285dd7099
wisestMe <wyhzest@gmail.com>
2021-01-07 08:01:23 +0100

    create instances of classes

--- IntroToCsharp/Program.cs
+++ IntroToCsharp/Program.cs
@@ -44,7 +44,10 @@ namespace IntroToCsharp
 
             Employee[] employees = new Employee[4];
 
-            //employees[1] = new Employee();
+            employees[0] = new Employee();
+            employees[1] = new PartTimeEmployee();
+            employees[2] = new FullTimeEmployee();
+            employees[3] = new TemporaryEmployee();
         }
     }
 }
