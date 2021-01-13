1c89382f2d4fc403e4815e00292c2625d2dfe8f8
wisestMe <wyhzest@gmail.com>
2021-01-13 10:32:17 +0100

    add properties

--- ACME.BL/Customer.cs
+++ ACME.BL/Customer.cs
@@ -8,5 +8,19 @@ namespace ACME.BL
 {
     public class Customer
     {
+        public int Id { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public string FullName
+        {
+            get 
+            {
+                return $"{LastName} {FirstName}";
+            }
+            
+        }
+
     }
 }
