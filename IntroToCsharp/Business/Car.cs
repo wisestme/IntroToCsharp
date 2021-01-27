0eecea2f7020d5c956c3b712199695d8d71b84a6
wisestMe <wyhzest@gmail.com>
2021-01-27 16:53:11 +0100

    add class

---
+++ IntroToCsharp/Business/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCsharp.Business
+{
+    class Car
+    {
+        public string CarOwner;
+        public string PaintColor;
+        public int MilesPerGallon;
+
+        public Car(string owner, string color, int mpg)
+        {
+            CarOwner = owner;
+            PaintColor = color;
+            MilesPerGallon = mpg;
+        }
+
+        public virtual int CalculateFuelForTrip(int tripDistance)
+        {
+            return tripDistance / MilesPerGallon;
+        }
+
+        public virtual void PrintCarDetails()
+        {
+            Console.WriteLine("---- Car Details ----");
+            Console.WriteLine($"Car Owner {CarOwner}");
+            Console.WriteLine($"Car colour: {PaintColor}");
+            Console.WriteLine($"Gas Milage: {MilesPerGallon}");
+
+        }
+    }
+}
