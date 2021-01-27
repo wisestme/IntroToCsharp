21e992379d2e5cc463604e04b23bd44c916fac8a
wisestMe <wyhzest@gmail.com>
2021-01-27 17:23:52 +0100

    derive from a class

---
+++ IntroToCsharp/Business/VolvoCar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCsharp.Business
+{
+    class VolvoCar : Car
+    {
+        public string VolvoSoundSystem;
+        public VolvoCar(string owner, string color, int mpg, string sound)
+            : base(owner, color, mpg)
+        {
+            VolvoSoundSystem = sound;
+        }
+
+        public override void PrintCarDetails()
+        {
+            base.PrintCarDetails();
+            Console.WriteLine($"Volvo Sound System is {VolvoSoundSystem}");
+        }
+    }
+}
