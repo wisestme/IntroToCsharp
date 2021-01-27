7ae6eb1b2bd4785ed6b2d6f2b751567c289ec19a
wisestMe <wyhzest@gmail.com>
2021-01-27 12:58:15 +0100

    add engine spec class

---
+++ IntroToCsharp/Services/EngineSpec.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCsharp.Services
+{
+    class EngineSpec
+    {
+        public int EngineCapacity;
+        public string FuelType;
+
+        public EngineSpec(int capacity, string type)
+        {
+            EngineCapacity = capacity;
+            FuelType = type;
+        }
+    }
+}
