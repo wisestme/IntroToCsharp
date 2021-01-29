85126d91ead02ce6035c57bfe65b7b83a3cb23e5
wisestMe <wyhzest@gmail.com>
2021-01-29 09:33:11 +0100

    create ford car class

---
+++ IntroToCsharp/Business/FordCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCsharp.Business
+{
+    class FordCar : Car
+    {
+        public string FordWheelsOption;
+
+        public FordCar(string owner, string color, int mpg, string wheels) : base(owner, color, mpg)
+        {
+            FordWheelsOption = wheels;
+        }
+    }
+}
