f62c83e68c91a5791479fc0e72a1fe7635b41baa
wisestMe <wyhzest@gmail.com>
2021-01-29 09:49:47 +0100

    inherit from volvo car

---
+++ IntroToCsharp/Business/VolvoW30.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCsharp.Business
+{
+    class VolvoW30 : VolvoCar
+    {
+        public VolvoW30(string owner, string color, int mpg, string sound) : base(owner, color, mpg, sound)
+        {
+
+        }
+    }
+}
