fe5785add036918361751231eedf4e83dd2718e0
wisestMe <wyhzest@gmail.com>
2021-01-29 10:04:08 +0100

    inherit from ford car

---
+++ IntroToCsharp/Business/FordWisest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCsharp.Business
+{
+    class FordWisest : FordCar
+    {
+        public FordWisest(string owner, string color, int mpg, string wheels) : base(owner, color, mpg, wheels)
+        {
+
+        }
+    }
+}
