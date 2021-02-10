1dd447b35354f3c7714a40836c5510f222126e92
wisestme01 <c.nwagwu@genesystechhub.com>
2021-02-10 15:58:11 +0100

    check calculator range

--- IntroToCsharp/Logic/Calculator.cs
+++ IntroToCsharp/Logic/Calculator.cs
@@ -8,10 +8,21 @@ namespace IntroToCsharp.Logic
 {
     class Calculator
     {
-
+        private bool RangeCheck(int num1, int num2)
+        {
+            return num1 > 0 && num1 < 100 && num2 > 0 && num2 < 100;
+        }
         public int CalculateProduct(int num1, int num2)
         {
-            return num1 * num2;
+            if (RangeCheck(num1, num2))
+            {
+                return num1 * num2;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
         }
     }
 }
