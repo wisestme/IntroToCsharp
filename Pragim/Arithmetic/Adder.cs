bcb4e1efd5c3a3ed554b6fb75cde261ebfc91d5a
wisestme01 <c.nwagwu@genesystechhub.com>
2021-02-17 11:25:34 +0100

    overload method

---
+++ Pragim/Arithmetic/Adder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pragim.Arithmetic
+{
+    class Adder
+    {
+        public void Add(int FN, int SN)
+        {
+            Console.WriteLine(FN + SN);
+        }
+
+        public void Add(int FN, int SN, int TN)
+        {
+
+        }
+
+        public void Add(float FN, float SN)
+        {
+
+        }
+
+    }
+}
